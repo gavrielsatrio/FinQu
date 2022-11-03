dbde3bc9ddb08e471342e909484cd96040844640
Gavriel Satrio Widjaya <gavriel228@gmail.com>
2022-11-03 12:52:44 +0700

    Add : Transaction model
    To store all the transaction information

---
+++ app/src/main/java/com/example/finqu/Model/Transaction.java
@@ -0,0 +1,27 @@
+package com.example.finqu.Model;
+
+import java.util.Date;
+
+public class Transaction {
+    public Date Date;
+    public String TransactionType;
+    public String Name;
+    public Boolean IsIn;
+    public Boolean IsOut;
+    public String PaidBy;
+    public String PaymentType;
+    public Boolean IsCheck;
+    public Integer Amount;
+
+    public Transaction(Date dateParam, String transactionTypeParam, String nameParam, Boolean isInParam, Boolean isOutParam, String paidByParam, String paymentTypeParam, Boolean isCheckParam, Integer amountParam) {
+        this.Date = dateParam;
+        this.TransactionType = transactionTypeParam;
+        this.Name = nameParam;
+        this.IsIn = isInParam;
+        this.IsOut = isOutParam;
+        this.PaidBy = paidByParam;
+        this.PaymentType = paymentTypeParam;
+        this.IsCheck = isCheckParam;
+        this.Amount = amountParam;
+    }
+}
