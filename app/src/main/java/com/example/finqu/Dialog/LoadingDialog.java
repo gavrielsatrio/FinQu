56a0dd94923647a26a79be9e3e665eb3962ebf1d
Gavriel Satrio Widjaya <gavriel228@gmail.com>
2022-11-03 20:44:50 +0700

    Add : Loading dialog
    To tell users that there is a loading process happening

---
+++ app/src/main/java/com/example/finqu/Dialog/LoadingDialog.java
@@ -0,0 +1,37 @@
+package com.example.finqu.Dialog;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.finqu.R;
+
+import androidx.appcompat.app.AlertDialog;
+
+public class LoadingDialog {
+    Context context;
+    private AlertDialog dialog;
+
+    public LoadingDialog(Context contextParam) {
+        this.context = contextParam;
+    }
+
+    public void ShowDialog(String descText) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+
+        View viewDialog = LayoutInflater.from(context).inflate(R.layout.dialog_loading_layout, null, false);
+        builder.setView(viewDialog);
+        builder.setCancelable(false);
+
+        dialog = builder.create();
+
+        ((TextView)viewDialog.findViewById(R.id.dialogLoadingLblDesc)).setText(descText);
+
+        dialog.show();
+    }
+
+    public void DismissDialog() {
+        dialog.dismiss();
+    }
+}
